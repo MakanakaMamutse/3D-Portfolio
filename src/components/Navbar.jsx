fe73728aebab53d8ed1540b2e2c3a8f784aa0e01
Makanaka <makanakamamutse@gmail.com>
2025-02-18 21:41:16 +0200

    Fixed mLogo import issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { mLogo, menu, close } from "../assets";
 
 
 const Navbar = () => {
@@ -22,7 +22,7 @@ const Navbar = () => {
               window.scrollTo(0, 0);
             }}
           >
-            <img src={logo} alt='logo' className='w-10 h-10 object-contain' />
+            <img src={`${import.meta.env.BASE_URL}${mLogo}`} alt='logo' className='w-10 h-10 object-contain' />
               <p className='text-white text-[18px] font-bold cursor-pointer flex'> Makanaka &nbsp; <span className='sm:block hidden'>| Mamutse</span> </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
