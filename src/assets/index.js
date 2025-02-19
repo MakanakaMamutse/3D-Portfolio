db1df8dfd933b543ac6b2e6aabf82c2b1f098b3b
Makanaka <makanakamamutse@gmail.com>
2025-02-19 05:28:24 +0200

    Added Vercel Analytics and some assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,13 +21,14 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import mySQL from "./tech/mySQL.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 import Shop2Shop from "./company/Shop2Shop.png";
-import Lead4Life from "./company/Lead4Life.png"
+import Lead4Life from "./company/Lead4Life.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
